6010e2c390a449a433c4bee37973a93651451126
Eric Kneiling <eric.kneiling@gmail.com>
2025-12-29 14:30:03 -0700

    init

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const { middleware, errorMiddleware } = require('@envoy/envoy-integrations-sdk');
+
+const app = express();
+app.use(middleware());
+
+app.post('/stay-duration', (req, res) => {
+  res.send([
+    {
+      label: 'How long can visitors stay? (in minutes)',
+      value: 60,
+      min: 0,
+      max: 180
+    },
+  ]);
+});
+
+function timeDeltaMinutes(date1, date2) {
+  const time1 = date1.getTime();
+  const time2 = date2.getTime();
+
+  const differenceInMilliseconds = Math.abs(time2 - time1);
+  const differenceInMinutes = differenceInMilliseconds / 60000;
+
+  return Math.floor(differenceInMinutes);
+}
+
+
+app.post('/visitor-sign-out', async (req, res) => {
+  const envoy = req.envoy; // our middleware adds an "envoy" object to req.
+  const job = envoy.job;
+  const allowedDuration = envoy.meta.config.ALLOWEDDURATION;
+  const visitor = envoy.payload;
+  const visitorName = visitor.attributes['full-name'];
+
+  const overstayed = timeDeltaMinutes(visitor.signedInAt, visitor.signedOutAt) > allowedDuration
+
+  const message = `${visitorName} stayed beyond their allotted time`;
+  await job.attach({ value: message });
+  
+  res.send();
+});
+
+app.use(errorMiddleware());
+
+const listener = app.listen(process.env.PORT || 0, () => {
+  console.log(`Listening on port ${listener.address().port}`);
+});
